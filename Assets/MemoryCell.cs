9ae92cd80b8edce747ed955cb7a9c79f46875cfe
Teraka <teraka91@gmail.com>
2018-11-20 14:43:15 +0100

    Initial commit

---
+++ Assets/MemoryCell.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCell : MonoBehaviour {
+
+
+    public bool testIncrement;
+    public bool testDecrement;
+
+    public GameObject wheelPrefab;
+    public Transform wheelHolder;
+    public Transform cylinder;
+    public List<NumberWheel> wheels;
+    public int value;
+    private int wheelCount;
+
+    public int Read() {
+        return value;
+    }
+
+	public void Increment() {
+        value += 1;
+        Refresh();
+    }
+
+    public void Decrement() {
+        if (value > 0)
+            value -= 1;
+        Refresh();
+    }
+
+    IEnumerator AddWheel() {
+        yield return 0;
+    }
+
+    IEnumerator RemoveWheel() {
+        yield return 0;
+    }
+
+    private void Refresh() {
+        wheelCount = Mathf.CeilToInt(Mathf.Log10(value + 1));
+        /*while (wheels.Count > 2 && wheels.Count > wheelCount) {
+            wheels[wheels.Count - 1].Despawn();
+            wheels.RemoveAt(wheels.Count - 1);
+        }
+        while (wheels.Count < wheelCount || wheels.Count < 2) {
+            GameObject newWheel = Instantiate(wheelPrefab, wheelHolder);
+            newWheel.transform.localPosition = new Vector3(0, 0, (wheels.Count - 1) * 0.35f);
+            newWheel.transform.localScale = new Vector3(0.8f, 0.8f, 1.0f);
+            wheels.Add(newWheel.GetComponent<NumberWheel>());
+            wheels[wheels.Count - 1].Spawn();
+        }*/
+        wheelHolder.localPosition = new Vector3(0, 0, -0.175f * (wheels.Count - 1));
+        cylinder.localScale = new Vector3(1.5f, 0.175f * (wheels.Count - 1) + 0.3f, 1.5f);
+        for (int i = 0; i < wheels.Count; i++) {
+            wheels[i].NewTarget((value / (int)Mathf.Pow(10, i)) % 10);
+        }
+    }
+
+    private void Start() {
+        wheels = new List<NumberWheel>();
+        Refresh();
+    }
+
+    private void Update() {
+        if (testDecrement) {
+            testDecrement = false;
+            Decrement();
+        }
+        if (testIncrement) {
+            testIncrement = false;
+            Increment();
+        }
+    }
+}
---
+++ Assets/NumberWheel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWheel : MonoBehaviour {
+
+    public int target;
+    public float smooth;
+    private bool moving = false;
+    public float origin;
+    private float destination;
+    private int state = 1; //0: stable, -1: destroying, 1: spawning
+    float velocity;
+
+    public void Spawn() {
+        state = 1;
+        origin = transform.localPosition.z;
+        destination = origin + 0.35f;
+    }
+
+    public void Despawn() {
+        state = -1;
+        destination = origin;
+    }
+
+    public void NewTarget(int target) {
+        this.target = target;
+        moving = true;
+    }
+    
+    void Update() {
+        if (moving) {
+            transform.eulerAngles = new Vector3(0, 0, Mathf.SmoothDampAngle(transform.localEulerAngles.z, 360 - target * 36, ref velocity, smooth));
+            if (velocity == 0)
+                moving = false;
+        }
+        /*if (state != 0) {
+            Debug.Log(transform.localPosition.z);
+            transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(0, 0, destination), 0.3f);
+            if (state == 1)
+                transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one, 0.3f);
+            else
+                transform.localScale = Vector3.Lerp(transform.localScale, Vector3.one * 0.8f, 0.3f);
+            if (Mathf.Abs(transform.localPosition.z - destination) < 0.001f) {
+                transform.localPosition = new Vector3(0, 0, destination);
+                transform.localScale = Vector3.one;
+                if (state == -1) {
+                    Destroy(gameObject);
+                }
+                state = 0;
+            }
+        }*/
+    }
+}
